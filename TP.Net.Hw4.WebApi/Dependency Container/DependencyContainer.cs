7d6492266000f573bf945cdbfab4c4e347c051ef
mhtaldmr <mht.alidemir@gmail.com>
2022-06-10 10:22:07 +0300

    WebApi dependency container added.

---
+++ TP.Net.Hw4.WebApi/Dependency Container/DependencyContainer.cs
@@ -0,0 +1,12 @@
+﻿namespace TP.Net.Hw4.WebApi.Dependency_Container
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddWebAPIServices(this IServiceCollection services,IConfiguration configuration)
+        {
+
+
+            return services;
+        }
+    }
+}
--- TP.Net.Hw4.WebApi/Program.cs
+++ TP.Net.Hw4.WebApi/Program.cs
@@ -1,6 +1,6 @@
 using MuhammetAliDemir.TP.Net.Hw4.Application.DependencyContainer;
 using MuhammetAliDemir.TP.Net.Hw4.Infrastructure.DependencyContainer;
-
+using TP.Net.Hw4.WebApi.Dependency_Container;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 //Adding containers for layers.
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddInfractructureServices(builder.Configuration);
+builder.Services.AddWebAPIServices(builder.Configuration);
 
 
 builder.Services.AddControllers();
